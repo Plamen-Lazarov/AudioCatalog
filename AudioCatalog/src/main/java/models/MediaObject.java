7bd5c4ec262929b446ac3dd3b23331a98cb88d90
Plamen Lazarov <123259263+Plamen-Lazarov@users.noreply.github.com>
2025-12-19 20:26:51 +0200

    Added abstract MediaObject
    Added MediaObject model with equals, hashCode and sorting logic

---
+++ AudioCatalog/src/main/java/models/MediaObject.java
@@ -0,0 +1,88 @@
+package models;
+
+import java.util.Objects;
+
+public abstract class MediaObject implements Comparable<MediaObject> {
+    private String title;
+    private String author;
+    private String genre;
+    private int year;
+    private int durationSeconds;
+
+    public MediaObject(String title, String author, String genre, int year, int durationSeconds) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.year = year;
+        this.durationSeconds = durationSeconds;
+    }
+    public abstract String getMediaType();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getDurationSeconds() {
+        return durationSeconds;
+    }
+
+    public void setDurationSeconds(int durationSeconds) {
+        this.durationSeconds = durationSeconds;
+    }
+
+    public String getFormattedDuration() {
+        return String.format("%02d:%02d", durationSeconds / 60, durationSeconds % 60);
+    }
+    @Override
+    public int compareTo(MediaObject other) {
+        return this.title.compareToIgnoreCase(other.title);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        MediaObject mediaObject = (MediaObject) other;
+        return year == mediaObject.year &&
+                Objects.equals(title, mediaObject.title) &&
+                Objects.equals(author, mediaObject.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s - %s (%d) | %s",
+                getMediaType(), author, title, year, getFormattedDuration());
+    }
+}
