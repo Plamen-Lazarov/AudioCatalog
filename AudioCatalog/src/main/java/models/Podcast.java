7879cf7d4e81c381800417bffeeef6154535f36b
Plamen Lazarov <123259263+Plamen-Lazarov@users.noreply.github.com>
2025-12-19 20:43:32 +0200

    Added models
    Added Song and Podcast models

---
+++ AudioCatalog/src/main/java/models/Podcast.java
@@ -0,0 +1,28 @@
+package models;
+
+public class Podcast extends MediaObject {
+    private int episodeNumber;
+
+    public Podcast(String title, String host, String genre, int year, int durationSeconds, int episodeNumber) {
+        super(title, host, genre, year, durationSeconds);
+        this.episodeNumber = episodeNumber;
+    }
+
+    public int getEpisodeNumber() {
+        return episodeNumber;
+    }
+
+    public void setEpisodeNumber(int episodeNumber) {
+        this.episodeNumber = episodeNumber;
+    }
+
+    @Override
+    public String getMediaType() {
+        return "Podcast";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " [Ep. #" + episodeNumber + "]";
+    }
+}
---
+++ AudioCatalog/src/main/java/models/Song.java
@@ -0,0 +1,28 @@
+package models;
+
+public class Song extends MediaObject {
+    private String album;
+
+    public Song(String title, String author, String genre, int year, int durationSeconds, String album) {
+        super(title, author, genre, year, durationSeconds);
+        this.album = album;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    @Override
+    public String getMediaType() {
+        return "Song";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " [Album: " + album + "]";
+    }
+}
